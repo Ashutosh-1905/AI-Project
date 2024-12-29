ed81eab09254dd70ad32ac64f0eba2facf2229ca
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2024-12-29 21:51:17 +0530

    change the authorizarition logic

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,15 +4,21 @@ import config from "../config/config.js";
 
 export const authUser = (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) {
-      return next(createHttpError(401, "Access Denied. No token provided."));
+    let token;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+      token = authHeader.split(" ")[1];
+
+      if (!token) {
+        return next(createHttpError(401, "Access Denied. No token provided."));
+      }
+
+      const decoded = jwt.verify(token, config.jwtSecret);
+      req.user = decoded;
+      next();
     }
-
-    const decoded = jwt.verify(token, config.jwtSecret);
-    req.user = decoded;
-    next();
   } catch (error) {
     return next(createHttpError(401, "Invalid or Expired Token."));
   }
